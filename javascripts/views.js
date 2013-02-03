fb124e33124d88b55eec54f1baa62ee178704c3e
Joris Geessels <joris.geessels@gmail.com>
2013-02-03 20:16:54 +0100

    partiall fixed code style views.js

--- javascripts/views.js
+++ javascripts/views.js
@@ -1,194 +1,202 @@
+/*jslint indent: 2, browser: true */
+/*global define, jQuery, $*/
 define('views',['backbone', 'underscore','mustache', 'q'],
-  function(Backbone, _, Mustache, Q) {
-    var module = function() {
+  function (Backbone, _, Mustache, Q) {
+    'use strict';
+    var module = function () {
       // TODO: StateView could be a mixin instead of a full blown object.
-      this.StateView = function(options){
-        Backbone.View.apply(this,[options]);
-      }
+      this.StateView = function (options) {
+        Backbone.View.apply(this, [options]);
+      };
 
       _.extend(
-        this.StateView.prototype, 
-        Backbone.View.prototype,{
-        tagName : 'div',
-
-        states : ['start'],
-
-        current_state : 'start',
-
-        transitions : {
-          '*' : {},
-        },
-
-        getCurrentState : function() {
-          return this.current_state;
-        },
-        
-        // callback should always return a promise.
-        setTransition : function(start_state, end_state, callback) {
-          if (($.inArray(start_state, this.states) != -1 || start_state == '*') && $.inArray(end_state, this.states) != -1) {
-            if (!this.transitions[start_state]) {
-              this.transitions[start_state] = {};
+        this.StateView.prototype,
+        Backbone.View.prototype,
+        {
+          tagName : 'div',
+          states : ['start'],
+          current_state : 'start',
+          transitions : {
+            '*' : {}
+          },
+          getCurrentState : function () {
+            return this.current_state;
+          },
+          // callback should always return a promise.
+          setTransition : function (start_state, end_state, callback) {
+            if (($.inArray(start_state, this.states) !== -1 || start_state === '*') && $.inArray(end_state, this.states) !== -1) {
+              if (!this.transitions[start_state]) {
+                this.transitions[start_state] = {};
+              }
+              this.transitions[start_state][end_state] = callback;
             }
-            this.transitions[start_state][end_state] = callback;
-          }
-        },
+          },
+          doTransition: function (next_state) {
+            var transitions, that, successcallback, promise;
+            transitions = this.transitions[this.current_state];
 
-        doTransition: function(next_state, quiet=false) {
-          var transitions = this.transitions[this.current_state];
-
-          var that = this;;
-          var successcallback = function(){
-            var prev_state = that.current_state;
-            that.current_state = next_state;
-            // Trigger the event.
-            if (!quiet){
+            that = this;
+            successcallback = function () {
+              var prev_state = that.current_state;
+              that.current_state = next_state;
+              // Trigger the event.
               that.trigger(prev_state + ':' + next_state);
+            };
+
+            if (transitions && transitions[next_state]) {
+              promise = transitions[next_state].call(this);
+              promise.done(successcallback);
+            } else if (this.transitions['*'][next_state]) {
+              promise = this.transitions['*'][next_state].call(this);
+              promise.done(successcallback);
+            } else {
+              promise = Q.reject('unvalid-state');
             }
-          }
-
-          if (transitions && transitions[next_state]) {
-            var callback = transitions[next_state];
-             var promise = callback.call(this);
-             promise.done(successcallback);
-             return promise;
-          } else if (this.transitions['*'][next_state]) {
-            var callback = this.transitions['*'][next_state];
-            var promise = callback.call(this);
-            promise.done(successcallback);
+            
             return promise;
+          },
+          render : function () {
+            var html, data;
+            data = this.preprocess(this.model);
+            data.state = this.current_state;
+            // use Mustache to render
+            if (this.template[this.current_state]) {
+              html = Mustache.render(this.template[this.current_state], data);
+            } else {
+              html = Mustache.render(this.template.default, data);
+            }
+            // replace the html of the element
+            $(this.el).html(html);
+            // return an instance of the view
+            return this;
           }
-          
-          return Q.reject('unvalid-state');
-        },
-
-        render : function(){
-          data =this.preprocess(this.model)
-          data.state = this.current_state;
-          // use Mustache to render
-          if (this.template[this.current_state]) {
-            html = Mustache.render(this.template[this.current_state],data);
-          } else {
-            html = Mustache.render(this.template.default,data);
-          }
-          // replace the html of the element
-          $(this.el).html(html);
-          // return an instance of the view
-          return this;
-        },
-      });
+        }
+      );
 
       this.StateView.extend = Backbone.View.extend;
       
-      this.ArticleView =function(options){
-        Backbone.View.apply(this,[options]);
+      this.ArticleView = function (options) {
+        Backbone.View.apply(this, [options]);
       };
 
       this.ArticleView.extend = Backbone.View.extend;
 
       _.extend(
-          this.ArticleView.prototype, 
-          this.StateView.prototype,{
+        this.ArticleView.prototype,
+        this.StateView.prototype,
+        {
+          template : {
+            default : '<div class="row"><div class="three columns placeholder"></div><div class="nine columns title"><h4><i class="foundicon-photo"></i> {{title}}</h4><ul class="meta"><p> {{summary}}</p>{{#meta}}<li class="{{name}}">{{{value}}}</li>{{/meta}}</ul></div><div class="nine columns body">{{#thumbnails}} <img src="{{src}}"/>{{/thumbnails}}</div></div></div>'
+          },
+          events : {
+            'click .title' : 'onTitleClick'
+          },
+          initialize : function () {
+            this.$el.toggleClass('start');
+            this.states.push('end');
+            this.states.push('open');
+            this.states.push('closed');
+            this.setTransition('start', 'open', function () {
+              var that, promise;
+              that = this;
+              _gaq.push(['_trackEvent', 'item', 'open', this.model.get('title')]);
+              that.$('.placeholder').animate({width: 0}, 500, 'linear', function () {
+                $(this).hide();
+              });
 
-        template : { 
-          default : '<div class="row"><div class="three columns placeholder"></div><div class="nine columns title"><h4><i class="foundicon-photo"></i> {{title}}</h4><ul class="meta"><p> {{summary}}</p>{{#meta}}<li class="{{name}}">{{{value}}}</li>{{/meta}}</ul></div><div class="nine columns body">{{#thumbnails}} <img src="{{src}}"/>{{/thumbnails}}</div></div></div>',
-        },
-
-        events : {
-          'click .title' : 'onTitleClick',
-        },
-
-        initialize : function() {
-          this.$el.toggleClass('start');
-          this.states.push('end');
-          this.states.push('open');
-          this.states.push('closed');
-          this.setTransition('start', 'open',function(successcallback){
-            var that = this;
-            _gaq.push(['_trackEvent', 'item', 'open', this.model.get('title')]);
-            that.$('.placeholder').animate({width: 0},500,'linear', function(){
-              $(this).hide();
-            });
-
-
-            this.model.fetch({
-              context : that,
-              success : function(){
-                successcallback.call(that); 
-                that.render();
-                that.$('.placeholder').hide();
-                that.$('.body').slideDown(500, function(){
-                  that.$('.meta').slideDown(500);
+              promise = this.model.fetch()
+                .then(function () {
+                  that.render();
+                  that.$('.placeholder').hide();
+                  var defferedpromise = Q.defer();
+                  that.$('.body').slideDown(500, function () {
+                    that.$('.meta').slideDown(500);
+                    // TODO: pass smth sensible as the resolve argument.
+                    defferedpromise.resolve(that);
+                  });
+                  return defferedpromise;
                 });
-              },
+              return promise;
             });
-          });
 
-          this.setTransition('closed', 'open',function(successcallback){
-            var that = this;
-            this.$('.placeholder').animate({width: 0},500,'linear',function(){ 
-              that.$('.body').slideDown(500,function(){
-                that.$('.meta').slideDown(200);
-                successcallback.call(that); 
+            this.setTransition('closed', 'open', function () {
+              var defferedpromise = Q.defer();
+              var that = this;
+              this.$('.placeholder').animate({width: 0}, 500, 'linear', function () {
+                that.$('.body').slideDown(500, function () {
+                  that.$('.meta').slideDown(200);
+                  defferedpromise.resolve(that);
+                });
+              });
+              return defferedpromise;
+            });
+
+            this.setTransition('open', 'closed', function () {
+              var that, defferedpromise;
+              that = this;
+              defferedpromise = Q.defer();
+
+              that.$('.meta').slideUp(200, function () {
+                that.$('.body').slideUp(500, function () {
+                  that.$('.placeholder').show();
+                  that.$('.placeholder').animate({width: '25%'}, 500, 'linear', function () {
+                    defferedpromise.resolve(that);
+                  });
+                });
+              });
+              return defferedpromise;
+            });
+
+            this.setTransition('*', 'end', function () {
+              var that, defferedpromise;
+              that = this;
+              defferedpromise = Q.defer();
+              this.$el.slideUp(500, 'linear', function () {
+                defferedpromise.resolve(that);
+                $(this).remove();
               });
             });
-          });
 
-          this.setTransition('open', 'closed',function(successcallback){
-            var that = this;
-            that.$('.meta').slideUp(200,function(){
-              that.$('.body').slideUp(500,function(){
-              that.$('.placeholder').show();
-              that.$('.placeholder').animate({width: '25%'},500,'linear', function(){
-                successcallback.call(that);
-              });
+            this.model.bind('destroy', this.close, this);
+          },
+          close : function () {
+            this.doTransition('end');
+          },
+          onTitleClick : function (event) {
+            if (this.current_state == 'start' || this.current_state == 'closed') {
+              this.doTransition('open');
+            } else {
+              this.doTransition('closed');
+            }
+          },
+          preprocess : function (model) {
+            var meta = [];
+            var created = model.get('created');
+            var updated = model.get('updated');
+            meta.push({
+              name : 'created',
+              value: "<i class='foundicon-clock'></i><span> " + created.day + " / " + created.month + " / " + created.year + "</span>"
             });
-           });
-          });
-
-          this.setTransition('*', 'end', function(successcallback) {
-            var that = this;
-            this.$el.slideUp(500,'linear',function() {
-              successcallback.call(that); 
-              $(this).remove();
+            meta.push({
+              name : 'updated',
+              value: "<i class='foundicon-edit'></i> <span> " + updated.day + " / " + updated.month + " / " + updated.year  + "</span>"
             });
-          });
 
-          this.model.bind('destroy', this.close, this);
-        },
-
-        close : function(){
-          this.doTransition('end');
-        },
-
-        onTitleClick : function(event) {
-          if (this.current_state == 'start' || this.current_state == 'closed') {
-            this.doTransition('open');
-          } else {
-            this.doTransition('closed');
+            return {
+              title : model.get('title'),
+              summary : model.get('description'),
+              meta : meta
+            };
           }
-        },
-        preprocess : function(model) {
-          var meta = [];
-          var created = model.get('created');
-          var updated = model.get('updated');
-          meta.push({name : 'created', value: "<i class='foundicon-clock'></i><span> " +created.day + " / " + created.month + " / " + created.year + "</span>" });
-          meta.push({name : 'updated', value: "<i class='foundicon-edit'></i> <span> " +updated.day + " / " + updated.month + " / " + updated.year  +"</span>"});
-
-          return {
-            title : model.get('title'),
-            summary : model.get('description'),
-            meta : meta,
-          };
-        },
-
-      });
+        }
+      );
 
       this.AlbumView = this.ArticleView.extend({
         template : {
-          default : '<div class="row"><div class="three columns placeholder"></div><div class="nine columns title"><h4><i class="foundicon-photo"></i> {{title}}</h4><ul class="meta"><p> {{summary}}</p>{{#meta}}<li class="{{name}}">{{{value}}}</li>{{/meta}}</ul></div><div class="nine columns body">{{#thumbnails}} <a href="{{src}}"><img src="{{thumbsrc}}"/></a>{{/thumbnails}}</div></div></div>',
+          default : '<div class="row"><div class="three columns placeholder"></div><div class="nine columns title"><h4><i class="foundicon-photo"></i> {{title}}</h4><ul class="meta"><p> {{summary}}</p>{{#meta}}<li class="{{name}}">{{{value}}}</li>{{/meta}}</ul></div><div class="nine columns body">{{#thumbnails}} <a href="{{src}}"><img src="{{thumbsrc}}"/></a>{{/thumbnails}}</div></div></div>'
         },
 
-        render : function(){
+        render : function () {
           data =this.preprocess(this.model)
           data.state = this.current_state;
           // use Mustache to render
